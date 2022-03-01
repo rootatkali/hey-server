fe4b0d0abb0e49cd055f62773741cde9219a6e67
Rotem Moses <rotem@faceco.de>
2022-03-01 18:07:51 +0200

    Create editUser method in UserService

--- src/main/java/me/rootatkali/hey/service/UserService.java
+++ src/main/java/me/rootatkali/hey/service/UserService.java
@@ -4,21 +4,23 @@ import me.rootatkali.hey.model.User;
 import me.rootatkali.hey.model.UserPreferences;
 import me.rootatkali.hey.repo.UserPreferencesRepository;
 import me.rootatkali.hey.repo.UserRepository;
+import me.rootatkali.hey.util.Xss;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class UserService {
   private final UserRepository userRepo;
   private final UserPreferencesRepository userPrefsRepo;
+  private final Validator validator;
   
   @Autowired
   public UserService(UserRepository userRepo,
-                     UserPreferencesRepository userPrefsRepo) {
+                     UserPreferencesRepository userPrefsRepo,
+                     Validator validator) {
     this.userRepo = userRepo;
     this.userPrefsRepo = userPrefsRepo;
+    this.validator = validator;
   }
   
   public Iterable<User> getUsers() {
@@ -41,8 +43,58 @@ public class UserService {
     return user.getPrefs();
   }
   
-  public User addUser(User u) {
-    // todo delete
-    return userRepo.save(u);
+  /**
+   * Provides a PATCH method for the user entity.
+   * @param db The user, as fetched from the database
+   * @param edit The requested edits to the user
+   * @return <code>db</code>, after being edited accordingly
+   */
+  public User editUser(User db, User edit) {
+    if (edit.getUsername() != null) {
+      validator.validateUsername(edit.getUsername());
+      db.setUsername(edit.getUsername());
+    }
+    
+    if (edit.getFirstName() != null) {
+      validator.validateName(edit.getFirstName());
+      db.setFirstName(Xss.deXss(edit.getFirstName()));
+    }
+    
+    if (edit.getLastName() != null) {
+      validator.validateName(edit.getLastName());
+      db.setFirstName(Xss.deXss(edit.getLastName()));
+    }
+    
+    if (edit.getPhoneNum() != null) {
+      validator.validatePhoneNumber(edit.getPhoneNum());
+      db.setPhoneNum(edit.getPhoneNum());
+    }
+    
+    if (edit.getEmail() != null) {
+      validator.validateEmail(edit.getEmail());
+      db.setEmail(edit.getEmail());
+    }
+    
+    if (edit.getHometown() != null) {
+      validator.validateName(edit.getHometown());
+      db.setHometown(edit.getHometown());
+    }
+    
+    if (edit.getGrade() != null) {
+      validator.validateGrade(edit.getGrade());
+      db.setGrade(edit.getGrade());
+    }
+    
+    if (edit.getGender() != null) {
+      validator.validateGender(edit.getGender());
+      db.setGender(edit.getGender());
+    }
+    
+    if (edit.getBio() != null) {
+      validator.validateBio(edit.getBio());
+      db.setBio(Xss.deXss(edit.getBio()));
+    }
+    
+    return userRepo.save(db);
   }
 }
--- src/main/java/me/rootatkali/hey/service/Validator.java
+++ src/main/java/me/rootatkali/hey/service/Validator.java
@@ -16,35 +16,41 @@ import java.util.regex.Pattern;
 public class Validator {
   private final UserRepository userRepo;
   
+  private static final int BIO_MAX_LENGTH = 5000;
+  
   @Autowired
   public Validator(UserRepository userRepo) {
     this.userRepo = userRepo;
   }
   
   public void validateUsername(String username) {
+    var invalidUsername = new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid username.");
+    
     // check length >= 3
-    if (username == null || username.length() < 3) throw Error.BAD_REQUEST.get();
+    if (username == null || username.length() < 3) throw invalidUsername;
     // check alphanum
-    if (!username.matches("^[a-zA-Z][a-zA-Z0-9]+$")) throw Error.BAD_REQUEST.get();
+    if (!username.matches("^[a-zA-Z][a-zA-Z0-9]+$")) throw invalidUsername;
     // check not already exists
     if (userRepo.existsByUsername(username)) throw Error.ALREADY_EXISTS.get();
   }
   
   public void validateName(String name) {
+    var invalidName = new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid name: " + name + ".");
+    
     final String approvedSpecialChars = " '-";
     // check for empty name
-    if (Strings.isNullOrEmpty(name)) throw Error.BAD_REQUEST.get();
+    if (Strings.isNullOrEmpty(name)) throw invalidName;
     // trim and check for printability
     name = name.strip();
     for (char c : name.toCharArray()) {
-      if (!(Character.isLetter(c) || approvedSpecialChars.contains("" + c))) throw Error.BAD_REQUEST.get();
+      if (!(Character.isLetter(c) || approvedSpecialChars.contains("" + c))) throw invalidName;
     }
   }
   
   public void validateEmail(String email) {
     // email regex from internet
     if (!Pattern.matches("^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$", email))
-      throw Error.BAD_REQUEST.get();
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid email address.");
   }
   
   /**
@@ -52,17 +58,18 @@ public class Validator {
    */
   @SuppressWarnings("UnstableApiUsage")
   public void validatePhoneNumber(String number) {
+    var invalidPhone = new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid phone number.");
     // check if 10-digit number
-    if (number == null || number.length() != 10) throw Error.BAD_REQUEST.get();
-    if (Longs.tryParse(number) == null) throw Error.BAD_REQUEST.get();
+    if (number == null || number.length() != 10) throw invalidPhone;
+    if (Longs.tryParse(number) == null) throw invalidPhone;
     
     // check for mobile carrier prefix
-    if (!number.startsWith("05")) throw Error.BAD_REQUEST.get();
-    if (!List.of('0', '1', '2', '3', '4', '5', '8').contains(number.charAt(2))) throw Error.BAD_REQUEST.get();
+    if (!number.startsWith("05")) throw invalidPhone;
+    if (!List.of('0', '1', '2', '3', '4', '5', '8').contains(number.charAt(2))) throw invalidPhone;
   }
   
   public void validatePassword(String password) {
-    ResponseStatusException invalidPassword = new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid password.");
+    var invalidPassword = new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid password.");
     
     // 8-64 chars
     if (password == null || password.length() < 8 || password.length() > 64) throw invalidPassword;
@@ -84,5 +91,19 @@ public class Validator {
     if (password.matches(".*[-,. !@#$%^&*()_+=?/\\[\\]{}\\\\].*")) checks++;
     
     if (checks < 3) throw invalidPassword;
+  }
+  
+  public void validateGrade(int grade) {
+    // includes support for grades 13 and 14
+    if (grade < 7 || grade > 14) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid grade.");
+  }
+  
+  public void validateGender(char gender) {
+    if (!"MFOX".contains(Character.toString(gender).toUpperCase()))
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid gender.");
+  }
+  
+  public void validateBio(String bio) {
+    if (bio.length() > BIO_MAX_LENGTH) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bio too long.");
   }
 }
