d47ae420128c7bf23dd1888a8310e4211caa6889
Rotem Moses <rotem@faceco.de>
2022-06-22 20:49:04 +0300

    logloglog

--- src/main/java/me/rootatkali/hey/chat/ChatNotification.java
+++ src/main/java/me/rootatkali/hey/chat/ChatNotification.java
@@ -1,4 +1,10 @@
 package me.rootatkali.hey.chat;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public record ChatNotification(String id, String senderId, String name) {
+  public ChatNotification {
+    log.info("notification " + id);
+  }
 }
